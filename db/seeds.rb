c63299521abc229024245ff801500cd7ca11c11b
ladl <lena-bitbucket@auf-der-landwehr.com>
2017-06-24 12:12:00 +0200

    seeds.rb adjusted to 2017

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ ChildAccount.delete_all
 MainAccount.create([{name: 'Lagerkasse Bar'}, {name: 'Gruppenleiterkasse Girokonto'}, {name: 'Gruppenleiterkasse'}, {name: 'Kinderkasse'}])
 
 User.delete_all
-User.create([{name: 'Johannes'}, {name: 'Tobias'}, {name: 'Gast'}])
+User.create([{name: 'Annalena'}, {name: 'Tobias'}, {name: 'Johannes'}, {name: 'Gast'}])
 
 Tent.delete_all
 Scout.delete_all
@@ -26,15 +26,15 @@ Child.delete_all
 
 Setting.delete_all
 Setting.create([ 
-    {key: :start_date, type: :date, value: "2016-07-14"},
-    {key: :end_date, type: :date, value: "2016-07-31"}
+    {key: :start_date, type: :date, value: "2017-07-13"},
+    {key: :end_date, type: :date, value: "2017-07-30"}
 ])
 
 Good.delete_all
-Good.create([{type: :beer, price: "0.7", date: "2016-01-01"},
-            {type: :soft_drink, price: "0.5", date: "2016-01-01"},
-            {type: :sausage, price: "1.0", date: "2016-01-01"},
-            {type: :pork, price: "2.0", date: "2016-01-01"},
-            {type: :turkey, price: "2.0", date: "2016-01-01"},
-            {type: :corn, price: "1.0", date: "2016-01-01"}
+Good.create([{type: :beer, price: "0.7", date: "2017-01-01"},
+            {type: :soft_drink, price: "0.5", date: "2017-01-01"},
+            {type: :sausage, price: "1.0", date: "2017-01-01"},
+            {type: :pork, price: "1.2", date: "2017-01-01"},
+            {type: :turkey, price: "2.0", date: "2017-01-01"},
+            {type: :corn, price: "1.0", date: "2017-01-01"}
  ])
